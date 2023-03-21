bdf9623f5bcbaf5923509484350c0de1e94ad525
esayasgit21 <Esayas.Gizaw@paccar.com>
2023-03-20 21:22:29 -0700

    twaeked app for heroku

--- ethiochurchsite/ethiochurchsite/settings.py
+++ ethiochurchsite/ethiochurchsite/settings.py
@@ -9,11 +9,15 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+import django_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath()))
 
 
 # Quick-start development settings - unsuitable for production
@@ -118,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS =[os.path.join(BASE_DIR,'static')]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedMainfestSaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -133,3 +140,5 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'egizaw926@gmail.com'
 EMAIL_HOST_PASSWORD = 'ioiexnvcuwjtlizy'
+
+django_heroku.settings(locals()) 
