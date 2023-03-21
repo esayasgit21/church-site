e266ae3dcc960f20a05a9adea4021d5b211bff89
esayasgit21 <Esayas.Gizaw@paccar.com>
2023-03-21 00:13:25 -0700

    add all heroku config file fix

--- churchwebsite/apps.py
+++ churchwebsite/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class ChurchwebsiteConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+    # default_auto_field = 'django.db.models.BigAutoField'
     name = 'churchwebsite'
