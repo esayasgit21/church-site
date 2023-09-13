60fbf97434d5688ffea8db532443b1dc4a7d8fe6
Esayas Gizaw <80485773+esayasgit21@users.noreply.github.com>
2023-09-12 21:20:27 -0700

    Update 0003_course_imagedata.py

--- churchwebsite/migrations/0003_course_imagedata.py
+++ churchwebsite/migrations/0003_course_imagedata.py
@@ -10,17 +10,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='Course',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('subject', models.CharField(max_length=120, verbose_name='Course Name')),
-                ('web_link', models.URLField(blank=True, verbose_name='Website Address')),
-                ('grade', models.CharField(max_length=40, verbose_name='Grade')),
-                ('description', models.TextField(blank=True)),
-                ('image', models.FileField(blank=True, null=True, upload_to='course/')),
-            ],
-        ),
+      
         migrations.CreateModel(
             name='ImageData',
             fields=[
